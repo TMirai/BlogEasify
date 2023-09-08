98e47fbce3586aea4e8f9a7b4b738e3ff6f8c848
katsuomi aoki <mugichak1227@gmail.com>
2023-09-08 13:04:47 +0900

    UI/UXの変更

--- src/components/pages/PhotoEdit.tsx
+++ src/components/pages/PhotoEdit.tsx
@@ -14,6 +14,9 @@ export const PhotoEdit = memo((props: Props) => {
     const canvasRef = useRef(null);
     const [colorvalue, setColorValue] = useState('black')
     const [positionvalue, setPositionValue] = useState('center')
+    const [fontValue, setFontValue] = useState('gothic')    
+    const [backValue, setBackValue] = useState('ari')        
+    const [isGoTitle, setIsGoTitle] = useState(false)
     const [ inputtitle, setInputitle ] = useRecoilState(Title);
     const [ edit, setEdit ] = useRecoilState(Edit);
     const [ editurl, setEditurl ] =useRecoilState(Editurl);
@@ -35,6 +38,7 @@ export const PhotoEdit = memo((props: Props) => {
             canvas.height = image.height;
             ctx.drawImage(image, 0, 0);
         };
+        setIsGoTitle(false);
     }
     function onClickDownload(){
         const canvas:any = canvasRef.current;
@@ -44,27 +48,36 @@ export const PhotoEdit = memo((props: Props) => {
         link.click();
     };
     const onClickInTitle = () =>{
+        setIsGoTitle(true);
         //キャンバスにテキストを描画する
         const canvas:any = canvasRef.current;
-        const ctx = canvas.getContext('2d');
+        const ctx = canvas.getContext('2d');   
         let fontColor = "#fff";
-        let backgroundColor = '#000';
+        let backgroundColor = '#000000CC';
         if (colorvalue == "white"){
             fontColor = "#000";
-            backgroundColor = '#fff';
+            backgroundColor = '#ffffffCC';
         } 
         let y = canvas.height/2;
         if(positionvalue == 'up'){
             y = 50;
         }else if(positionvalue == "down"){
-            y = 350;
+            y = 400;
         }
 
         const txw = ctx.measureText(text);
-        ctx.font = '25px Roboto medium';
-        // 背景を描画
-        ctx.fillStyle = backgroundColor;
-        ctx.fillRect((canvas.width - txw.width) / 2 - txw.width, y-30, 3*txw.width, 50);
+        ctx.font = "30px Arial";
+        if(fontValue === 'mincho'){
+            ctx.font = '30px Roboto medium';
+        }
+        
+        if(backValue === 'ari'){
+            // 背景を描画
+            ctx.fillStyle = backgroundColor;
+            console.log(canvas.width)
+            console.log(txw.width)
+            ctx.fillRect(0, y-50, canvas.width, 80);
+        }
         ctx.fillStyle = fontColor;
         ctx.textBaseline = 'center';
         ctx.textAlign = 'center';
@@ -83,22 +96,40 @@ export const PhotoEdit = memo((props: Props) => {
     return (
         <div>
             <div>
-                <canvas ref={canvasRef} id='preview' style={{width: '700px'}}></canvas>
+                <canvas ref={canvasRef} id='preview' style={{width: '700px', height: '394px'}}></canvas>
             </div>
             <div style={{textAlign: 'right', marginTop: '10px'}}>
                 <RadioGroup onChange={setColorValue} value={colorvalue}>
                     <Center>
                         <Stack direction='row'>
-                            <span style={{color: 'black'}}>色：</span>
-                            <Radio value='black'><span style={{color: 'black'}}>黒ベース</span></Radio>
-                            <Radio value='white'><span style={{color: 'black'}}>白ベース</span></Radio>
+                            <span style={{color: 'black'}}>文字色:</span>
+                            <Radio value='black'><span style={{color: 'black'}}>白</span></Radio>
+                            <Radio value='white'><span style={{color: 'black'}}>黒</span></Radio>
+                        </Stack>
+                    </Center>
+                </RadioGroup>
+                <RadioGroup onChange={setBackValue} value={backValue}>
+                    <Center>
+                        <Stack direction='row'>
+                            <span style={{color: 'black'}}>文字背景:</span>
+                            <Radio value='ari'><span style={{color: 'black'}}>あり</span></Radio>
+                            <Radio value='nashi'><span style={{color: 'black'}}>なし</span></Radio>
+                        </Stack>
+                    </Center>
+                </RadioGroup>                
+                <RadioGroup onChange={setFontValue} value={fontValue}>    
+                    <Center>           
+                        <Stack direction='row'>
+                            <span style={{color: 'black'}}>フォント:</span>     
+                            <Radio value='gothic'><span style={{color: 'black'}}>ゴシック体</span></Radio>
+                            <Radio value='mincho'><span style={{color: 'black'}}>明朝体</span></Radio>
                         </Stack>
                     </Center>
                 </RadioGroup>
                 <RadioGroup onChange={setPositionValue} value={positionvalue}>    
                     <Center>           
                         <Stack direction='row'>
-                            <span style={{color: 'black'}}>位置：</span>     
+                            <span style={{color: 'black'}}>位置:</span>     
                             <Radio value='up'><span style={{color: 'black'}}>上</span></Radio>
                             <Radio value='center'><span style={{color: 'black'}}>真ん中</span></Radio>
                             <Radio value='down'><span style={{color: 'black'}}>下</span></Radio>
@@ -106,7 +137,8 @@ export const PhotoEdit = memo((props: Props) => {
                     </Center>
                 </RadioGroup>
                 <p>
-                    <Button size='xs' bg="teal.400" color="white" _hover={{ opacity: 0.8 }} onClick={onClickInTitle}>タイトル挿入</Button>
+                    {isGoTitle && <Button size='xs' bg="Gray" color="white" _hover={{ opacity: 0.8 }}>タイトル挿入</Button>}
+                    {!isGoTitle && <Button size='xs' bg="teal.400" color="white" _hover={{ opacity: 0.8 }} onClick={onClickInTitle}>タイトル挿入</Button>}
                     <Button size='xs' bg="teal.400" ml={3} color="white" _hover={{ opacity: 0.8 }} onClick={loadImage}>タイトル削除</Button>   
                 </p>
                 <br />
